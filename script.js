7a7257dd0cc389da17350ad22cb44a7df186fc86
asde coeur <12coeur@gmail.com>
2025-09-26 19:22:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const viewer = new Cesium.Viewer("map", {
-  terrainProvider: Cesium.createWorldTerrain(),
-  timeline: false,
-  animation: false,
-  baseLayerPicker: false
-});
-const viewer = new Cesium.Viewer("map", {
-  imageryProvider: new Cesium.UrlTemplateImageryProvider({
-    url : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-  }),
-  baseLayerPicker: false
-});
+
